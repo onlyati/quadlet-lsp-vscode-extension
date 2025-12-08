96cd59ff08dd0fefc106ddbfb2d909b900803a69
ieris19 <developer@ieris19.com>
2025-12-08 17:33:22 +0100

    Improved the download script (#3)
    * Improved script to download quadlet-lsp artifacts
    
    Improvements to portability and error-resilience
    overall. Reduced code duplication using a loop
    
    * Removed build artifacts from git repository:
    
    Since the artifacts can be downloaded, it doesn't
    make sense to include them in the git repository.
    
    The .gitignore is updated to reflect this
    
    * Included darwin-arm64 in the quadlet-lsp download
    
    It was oddly missing, but its a target of both
    projects, so I added it.

--- bin/download.sh
+++ bin/download.sh
@@ -1,33 +1,70 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
+# ------------------------------------------------------------------------------
+# Configuration
+# ------------------------------------------------------------------------------
 target_version="0.6.0"
+target_architectures=("darwin-amd64" "darwin-arm64" "linux-amd64" "linux-arm64" "windows-amd64" "windows-arm64")
+target_url="https://github.com/onlyati/quadlet-lsp/releases/download/v${target_version}"
 
-cd ./darwin-amd64
-wget "https://github.com/onlyati/quadlet-lsp/releases/download/v${target_version}/quadlet-lsp-${target_version}-darwin-amd64.tar.gz"
-ls -1 | grep '.tar.gz' | xargs tar -xvf
-ls -1 | grep '.tar.gz' | xargs rm
-cd ..
+# ------------------------------------------------------------------------------
+# Checking prerequisite tools
+# ------------------------------------------------------------------------------
+required_tools=("wget" "tar" "unzip")
+for tool in "${required_tools[@]}"; do
+  if ! command -v "$tool" &> /dev/null; then
+    echo "Error: $tool is not installed. Please install it and try again."
+    exit 1
+  fi
+done
 
-cd ./linux-amd64
-wget "https://github.com/onlyati/quadlet-lsp/releases/download/v${target_version}/quadlet-lsp-${target_version}-linux-amd64.tar.gz"
-ls -1 | grep '.tar.gz' | xargs tar -xvf
-ls -1 | grep '.tar.gz' | xargs rm
-cd ..
+# ------------------------------------------------------------------------------
+# Downloading and extracting the files fore each architecture
+# ------------------------------------------------------------------------------
+for arch in "${target_architectures[@]}"; do
+  # If the directory does not exist, we create it
+  if [ ! -d "./${arch}" ]; then
+    mkdir "./${arch}"
+  fi
 
-cd ./linux-arm64
-wget "https://github.com/onlyati/quadlet-lsp/releases/download/v${target_version}/quadlet-lsp-${target_version}-linux-arm64.tar.gz"
-ls -1 | grep '.tar.gz' | xargs tar -xvf
-ls -1 | grep '.tar.gz' | xargs rm
-cd ..
+  # We use a sub-shell to change the directory context for each architecture
+  (
+    cd "./${arch}" || {
+      # We handle the case where changing directory fails to prevent issues.
+      echo "Failed to change directory to ./${arch}"
+      exit 1
+    }
 
-cd ./windows-amd64
-wget "https://github.com/onlyati/quadlet-lsp/releases/download/v${target_version}/quadlet-lsp-${target_version}-windows-amd64.zip"
-ls -1 | grep '.zip' | xargs unzip -o
-ls -1 | grep '.zip' | xargs rm
-cd ..
+    # We wipe any pre-existing artifacts in the folder
+    rm -f ./*
 
-cd ./windows-arm64
-wget "https://github.com/onlyati/quadlet-lsp/releases/download/v${target_version}/quadlet-lsp-${target_version}-windows-arm64.zip"
-ls -1 | grep '.zip' | xargs unzip -o
-ls -1 | grep '.zip' | xargs rm
-cd ..
+    # We handle the different archive formats based on the architecture
+    case "${arch}" in
+    windows-*)
+      file_extension=".zip"
+    ;;
+    *)
+      file_extension=".tar.gz"
+    ;;
+    esac
+
+    target_file="quadlet-lsp-${target_version}-${arch}${file_extension}"
+
+    # Download the appropriate file
+    wget "${target_url}/${target_file}" || exit 1
+
+    # Extract the downloaded file based on the file extension
+    case "${file_extension}" in
+    ".zip")
+      unzip "${target_file}" || exit 1
+    ;;
+    ".tar.gz")
+      tar -xzf "${target_file}" || exit 1
+    ;;
+    esac
+
+    # We remove the downloaded archive
+    rm -f "${target_file}"
+  )
+done
