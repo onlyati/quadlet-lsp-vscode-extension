8c71b030885d5914b831889a454409ca8e04efce
onlyati <onlyati+github@proton.me>
2026-06-29 19:31:24 +0200

    upgrade to v0.7.4 language server (#14)

--- bin/download.sh
+++ bin/download.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # ------------------------------------------------------------------------------
 # Configuration
 # ------------------------------------------------------------------------------
-target_version="0.7.3"
+target_version="0.7.4"
 target_architectures=("darwin-amd64" "darwin-arm64" "linux-amd64" "linux-arm64" "windows-amd64" "windows-arm64")
 target_url="https://github.com/onlyati/quadlet-lsp/releases/download/v${target_version}"
 
